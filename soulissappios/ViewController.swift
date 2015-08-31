9aa22a06af753b3ca2d7f21eaecb29b4aa66cf42
hatemalimam <me@hatemalimam.com>
2015-08-31 10:35:07 +0200

    Initial commit

---
+++ soulissappios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soulissappios
+//
+//  Created by Hatem Alimam on 31/08/15.
+//  Copyright (c) 2015 Souliss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
